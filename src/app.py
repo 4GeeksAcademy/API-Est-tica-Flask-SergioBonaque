89c414e1d3482aa943eb582f96e7a8738d6c1151
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2026-01-04 17:06:43 +0000

    da error

--- src/app.py
+++ src/app.py
@@ -65,6 +65,10 @@ def add_member():
 @app.route('/members/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     result = jackson_family.delete_member(member_id)
+if not result:
+        return jsonify({"error": "Miembro no encontrado"}), 404
+    return jsonify({"done": True}), 200
+
 
 
 # This only runs if `$ python src/app.py` is executed
